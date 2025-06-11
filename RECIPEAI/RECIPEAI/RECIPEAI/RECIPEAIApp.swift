43741e45cff1acf60b96a7ce19c7356423e6c496
Regan <regan_li@sfu.ca>
2025-06-11 00:22:33 -0700

    Initial Commit

---
+++ RECIPEAI/RECIPEAI/RECIPEAI/RECIPEAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RECIPEAIApp.swift
+//  RECIPEAI
+//
+//  Created by Regan on 2025-06-11.
+//
+
+import SwiftUI
+
+@main
+struct RECIPEAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RECIPEAI/RECIPEAI/RECIPEAITests/RECIPEAITests.swift
@@ -0,0 +1,17 @@
+//
+//  RECIPEAITests.swift
+//  RECIPEAITests
+//
+//  Created by Regan on 2025-06-11.
+//
+
+import Testing
+@testable import RECIPEAI
+
+struct RECIPEAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
